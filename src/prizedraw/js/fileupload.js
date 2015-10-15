2c5f30f876229fab5d1ab00761779caf1af98e38
chuanwu <chuanwusun@gmail.com>
2015-10-15 10:22:56 +0800

    x

--- src/prizedraw/js/fileupload.js
+++ src/prizedraw/js/fileupload.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
         $.ajax({
                 type    :   "GET",
                 url     :   "http://test.daji.im/api/misc/phone_and_code_list/",
-                dataType:  "jsonp",
+                dataType:   "jsonp",
                 complete: function(d){
-                      console.log(phone)
-                      console.log(code)
                 }
             });
 });
