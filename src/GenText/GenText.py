ed64345b0fd6d0913058cc4e39fb071128f693c1
chuanwu <chuanwusun@gmail.com>
2015-12-17 10:27:01 +0800

    x

--- src/GenText/GenText.py
+++ src/GenText/GenText.py
@@ -7,13 +7,14 @@ try:
 except ImportError:
     import Image, ImageDraw, ImageFont, ImageEnhance
 
-def GetTextImg(text,num=0):
+def GetTextImg(text,num=0,height=5000):
 
-    im = Image.new("RGB", (1600, 800), (234, 233, 233))
+    # withd and heght
+    im = Image.new("RGB", (2600, height), (234, 233, 233))
     dr = ImageDraw.Draw(im)
-    font = ImageFont.truetype(os.path.join(os.path.dirname(os.path.abspath(__file__)), "fz.TTF"), 500)
+    font = ImageFont.truetype(os.path.join(os.path.dirname(os.path.abspath(__file__)), "fz.TTF"), size=800)
 
-    dr.text((150, 140), text, font=font, fill="#555555")
+    dr.text((150, 160), text, font=font, fill="#555555")
 
     filename = str(num) + ".png"
     im.save(os.path.join(os.path.dirname(os.path.abspath(__file__)), filename))
@@ -24,16 +25,13 @@ def GetTextImg(text,num=0):
 if __name__=="__main__":
     f = open('namelist','r')
     i = 0
+    name = ""
     for line in f.readlines():
         i = i + 1
         if line:
-            GetTextImg(line.decode('utf-8'),i)
-            print line
+            name = name + line + '            \n'
+#            GetTextImg(line.decode('utf-8'),i)
         else:
             break
-    namelist=[u"文",u"周",u"朱",u"崔",u"金",u"董",u"许",u"黄",u"俞",u"赵",u"陈",u"孙"]
-    i = 0
-    for name in namelist:
-        i = i + 1
-#        GetTextImg(name,i)
 
+    GetTextImg(name.decode('utf-8'),2000,150000)
---
+++ src/prizedraw/prizedraw_name/js/fileupload.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $.ajax({
+                type    :   "GET",
+                url     :   "http://test.daji.im/api/misc/phone_and_code_list/",
+                dataType:   "jsonp",
+                complete: function(d){
+                }
+            });
+});
